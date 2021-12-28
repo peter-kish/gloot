595fda1b2274431086b71e20ed16f9ba076f1594
Peter Kish <pkish023@gmail.com>
2021-12-28 14:58:51 +0100

    [InventoryItemStackable] Added

---
+++ inventory_item_stackable.gd
@@ -0,0 +1,25 @@
+extends InventoryItem
+class_name InventoryItemStackable
+
+
+export(int) var stack_size = 1 setget _set_stack_size;
+
+
+func get_weight() -> float:
+    return stack_size;
+
+
+func _set_stack_size(new_stack_size: int) -> void:
+    assert(new_stack_size >= 1, "Stack size must be greater or equal to 1!");
+    stack_size = new_stack_size;
+    emit_signal("weight_changed", stack_size);
+    
+    
+func split(new_stack_size: int) -> bool:
+    assert(new_stack_size >= 1, "New stack size must be greater or equal to 1!");
+    assert(new_stack_size < stack_size, "New stack size must be smaller than the original stack size!");
+
+    var new_item = duplicate();
+    new_item._set_stack_size(new_stack_size);
+    _set_stack_size(stack_size - new_stack_size);
+    return get_inventory().add_item(new_item);
--- inventory_system.gd
+++ inventory_system.gd
@@ -4,6 +4,7 @@ extends EditorPlugin
 
 func _enter_tree():
     add_custom_type("InventoryItem", "Node", preload("inventory_item.gd"), null);
+    add_custom_type("InventoryItemStackable", "Node", preload("inventory_item_stackable.gd"), null);
     add_custom_type("Inventory", "Node", preload("inventory.gd"), null);
     add_custom_type("InventoryLimited", "Node", preload("inventory_limited.gd"), null);
 
