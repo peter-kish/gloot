7432f034f6e7e6f49ee2755cc153546a7581ab30
Peter Kish <pkish023@gmail.com>
2021-12-28 13:55:36 +0100

    [InventoryItem] Added get_weight() and get_inventory()

--- inventory_item.gd
+++ inventory_item.gd
@@ -4,3 +4,14 @@ class_name InventoryItem
 
 export(String) var item_name;
 onready var sprite = $Sprite;
+
+
+func get_weight() -> float:
+    return 1.0;
+
+
+func get_inventory() -> Node:
+    if !get_parent() is Inventory:
+        return null;
+        
+    return get_parent();
