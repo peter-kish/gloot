9d9396da03ecad70f63c32110e8c75327ea5c5a7
Peter Kish <pkish023@gmail.com>
2022-05-03 19:03:06 +0200

    [InventoryGrid] Added moving items around

--- inventory_grid.gd
+++ inventory_grid.gd
@@ -39,6 +39,16 @@ func add_item_at(item: InventoryItemRect, x: int, y: int) -> bool:
     return false;
 
 
+func move_item(item: InventoryItemRect, x: int, y: int) -> bool:
+    if rect_free(x, y, item.width, item.height, item):
+        item.x = x;
+        item.y = y;
+        emit_signal("contents_changed");
+        return true;
+
+    return false;
+
+
 func transfer(item: InventoryItem, destination: Inventory) -> bool:
     assert(false, "Can't use transfer() with InventoryGrid! Use transfer_to() instead!");
     return false;
@@ -51,13 +61,15 @@ func transfer_to(item: InventoryItemRect, destination: InventoryGrid, x: int, y:
     return false;
 
 
-func rect_free(x: int, y: int, w: int, h: int) -> bool:
+func rect_free(x: int, y: int, w: int, h: int, exception: InventoryItemRect = null) -> bool:
     if x + w > width:
         return false;
     if y + h > height:
         return false;
 
     for item in get_items():
+        if item == exception:
+            continue;
         var rect1: Rect2 = Rect2(Vector2(x, y), Vector2(w, h));
         var rect2: Rect2 = Rect2(Vector2(item.x, item.y), Vector2(item.width, item.height));
         if rect1.intersects(rect2):
@@ -75,7 +87,7 @@ func find_free_place(item: InventoryItemRect) -> Dictionary:
     return {};
 
 
-static func sort_items(item1: InventoryItemRect, item2: InventoryItemRect) -> bool:
+static func _sort_items(item1: InventoryItemRect, item2: InventoryItemRect) -> bool:
     var rect1: Rect2 = Rect2(Vector2(item1.x, item1.y), Vector2(item1.width, item1.height));
     var rect2: Rect2 = Rect2(Vector2(item2.x, item2.y), Vector2(item2.width, item2.height));
     return rect1.get_area() > rect2.get_area();
@@ -85,7 +97,7 @@ func sort() -> void:
     var item_array: Array;
     for item in get_items():
         item_array.append(item);
-    item_array.sort_custom(self, "sort_items");
+    item_array.sort_custom(self, "_sort_items");
 
     for item in get_items():
         remove_child(item);
@@ -95,6 +107,3 @@ func sort() -> void:
         assert(free_place.x != null, "Unexpected error in sort()! No free space!");
         assert(free_place.y != null, "Unexpected error in sort()! No free space!");
         add_item_at(item, free_place.x, free_place.y);
-        item_array.remove(item);
-
-    assert(item_array.size() == 0, "Unexpected error in sort()! Leftover items!");
