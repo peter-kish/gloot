773f9b37a000bfddef03806963a0c3a35deb68f8
Peter Kish <pkish023@gmail.com>
2022-05-20 22:21:36 +0200

    Fixed a crash

--- inventory_system.gd
+++ inventory_system.gd
@@ -10,7 +10,6 @@ func _enter_tree():
 
     add_custom_type("InventoryItem", "Node", preload("inventory_item.gd"), preload("images/icon_item.svg"));
     add_custom_type("InventoryItemStackable", "Node", preload("inventory_item_stackable.gd"), preload("images/icon_item_stackable.svg"));
-    add_custom_type("InventoryItemWeight", "Node", preload("inventory_item_weight.gd"), preload("images/icon_item_weight.svg"));
     add_custom_type("InventoryItemRect", "Node", preload("inventory_item_rect.gd"), preload("images/icon_item_rect.svg"));
 
     add_custom_type("Inventory", "Node", preload("inventory.gd"), preload("images/icon_inventory.svg"));
@@ -25,7 +24,6 @@ func _exit_tree():
 
     remove_custom_type("InventoryItem");
     remove_custom_type("InventoryItemStackable");
-    remove_custom_type("InventoryItemWeight");
     remove_custom_type("InventoryItemRect");
 
     remove_custom_type("Inventory");
