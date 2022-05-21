b3936c91c076400751342b5ebf9fd32aa961c581
Peter Kish <pkish023@gmail.com>
2022-05-21 20:07:48 +0200

    [InventoryGrid] Made the size and position getters public

--- inventory_grid.gd
+++ inventory_grid.gd
@@ -12,12 +12,20 @@ export(int) var height: int = 10 setget _set_height;
 var item_positions: Dictionary = {};
 
 
-func _get_item_position(item: InventoryItem) -> Vector2:
+static func get_type() -> String:
+    return "grid";
+
+
+static func get_item_script() -> Script:
+    return preload("inventory_item_rect.gd");
+
+
+func get_item_position(item: InventoryItem) -> Vector2:
     assert(item_positions.has(item), "The inventory does not contain this item!");
     return item_positions[item];
 
 
-func _get_item_size(item: InventoryItemRect) -> Vector2:
+func get_item_size(item: InventoryItemRect) -> Vector2:
     if item_definitions:
         var width = item_definitions.get_item_property(item.item_id, KEY_WIDTH, 1.0);
         var height = item_definitions.get_item_property(item.item_id, KEY_HEIGHT, 1.0);
@@ -28,15 +36,7 @@ func _get_item_size(item: InventoryItemRect) -> Vector2:
         return Vector2(width, height);
     else:
         return Vector2(1, 1);
-
-
-static func get_type() -> String:
-    return "grid";
-
-
-static func get_item_script() -> Script:
-    return preload("inventory_item_rect.gd");
-
+    
 
 func _ready():
     if !_is_sorted():
@@ -58,12 +58,13 @@ func _set_height(new_height: int) -> void:
 
 
 func add_item(item: InventoryItem) -> bool:
-    assert(item is InventoryItemRect, "InventoryGrid can only hold InventoryItemRect")
-    return add_item_at(item, 0, 0);
+    assert(item is InventoryItemRect, "InventoryGrid can only hold InventoryItemRect");
+    var free_place = find_free_place(item);
+    return add_item_at(item, free_place.x, free_place.y);
 
 
 func add_item_at(item: InventoryItemRect, x: int, y: int) -> bool:
-    var item_size = _get_item_size(item);
+    var item_size = get_item_size(item);
     if rect_free(x, y, item_size.x, item_size.y):
         item_positions[item] = Vector2(x, y);
         return .add_item(item);
@@ -80,7 +81,7 @@ func remove_item(item: InventoryItem) -> bool:
 
 
 func move_item(item: InventoryItemRect, x: int, y: int) -> bool:
-    var item_size = _get_item_size(item);
+    var item_size = get_item_size(item);
     if rect_free(x, y, item_size.x, item_size.y, item):
         item_positions[item] = Vector2(x, y);
         emit_signal("contents_changed");
@@ -94,7 +95,7 @@ func transfer(item: InventoryItem, destination: Inventory) -> bool:
 
 
 func transfer_to(item: InventoryItemRect, destination: InventoryGrid, x: int, y: int) -> bool:
-    var item_size = _get_item_size(item);
+    var item_size = get_item_size(item);
     if destination.rect_free(x, y, item_size.x, item_size.y):
         return .transfer(item, destination);
 
@@ -110,8 +111,8 @@ func rect_free(x: int, y: int, w: int, h: int, exception: InventoryItemRect = nu
     for item in get_items():
         if item == exception:
             continue;
-        var item_pos: Vector2 = _get_item_position(item);
-        var item_size: Vector2 = _get_item_size(item);
+        var item_pos: Vector2 = get_item_position(item);
+        var item_size: Vector2 = get_item_size(item);
         var rect1: Rect2 = Rect2(Vector2(x, y), Vector2(w, h));
         var rect2: Rect2 = Rect2(item_pos, item_size);
         if rect1.intersects(rect2):
@@ -121,7 +122,7 @@ func rect_free(x: int, y: int, w: int, h: int, exception: InventoryItemRect = nu
 
 
 func find_free_place(item: InventoryItemRect) -> Dictionary:
-    var item_size = _get_item_size(item);
+    var item_size = get_item_size(item);
     for y in range(width - (item_size.x - 1)):
         for x in range(height - (item_size.y - 1)):
             if rect_free(x, y, item_size.x, item_size.y):
@@ -131,8 +132,8 @@ func find_free_place(item: InventoryItemRect) -> Dictionary:
 
 
 func _sort_items(item1: InventoryItemRect, item2: InventoryItemRect) -> bool:
-    var rect1: Rect2 = Rect2(_get_item_position(item1), _get_item_size(item1));
-    var rect2: Rect2 = Rect2(_get_item_position(item2), _get_item_size(item2));
+    var rect1: Rect2 = Rect2(get_item_position(item1), get_item_size(item1));
+    var rect2: Rect2 = Rect2(get_item_position(item2), get_item_size(item2));
     return rect1.get_area() > rect2.get_area();
 
 
@@ -156,8 +157,8 @@ func sort() -> bool:
 
 func _is_sorted() -> bool:
     for item in get_items():
-        var item_pos = _get_item_position(item);
-        var item_size = _get_item_size(item);
+        var item_pos = get_item_position(item);
+        var item_size = get_item_size(item);
         if !rect_free(item_pos.x, item_pos.y, item_size.x, item_size.y, item):
             return false;
 
