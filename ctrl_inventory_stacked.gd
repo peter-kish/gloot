ef19d53ac2c69a6e976b46cc93e0c13fea4318b7
Peter Kish <pkish023@gmail.com>
2022-06-04 09:17:29 +0200

    [CtrlInventoryStacked] Updating the progress bar visibility

--- ctrl_inventory_stacked.gd
+++ ctrl_inventory_stacked.gd
@@ -2,14 +2,26 @@ class_name CtrlInventoryStacked
 extends CtrlInventory
 tool
 
-export(bool) var percent_visible = true;
+export(bool) var progress_bar_visible = true setget _set_progress_bar_visible;
+export(bool) var percent_visible = true setget _set_percent_visible;
 var progress_bar: ProgressBar;
+
+
+func _set_progress_bar_visible(new_progress_bar_visible: bool) -> void:
+    progress_bar_visible = new_progress_bar_visible
+    progress_bar.visible = progress_bar_visible;
+
+
+func _set_percent_visible(new_percent_visible: bool) -> void:
+    percent_visible = new_percent_visible;
+    progress_bar.percent_visible = percent_visible;
 
 
 func _ready():
     progress_bar = ProgressBar.new();
     progress_bar.size_flags_horizontal = SIZE_EXPAND_FILL;
     progress_bar.percent_visible = percent_visible;
+    progress_bar.visible = progress_bar_visible;
     add_child(progress_bar);
 
 
@@ -28,6 +40,7 @@ func _refresh():
     if progress_bar:
         var inventory_stacked: InventoryStacked = inventory;
         progress_bar.percent_visible = percent_visible;
+        progress_bar.visible = progress_bar_visible;
         progress_bar.min_value = 0;
         progress_bar.max_value = inventory_stacked.capacity;
         progress_bar.value = inventory_stacked.occupied_space;
