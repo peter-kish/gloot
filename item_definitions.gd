e487265744981d0157edd937d826b76cac4128b3
Peter Kish <pkish023@gmail.com>
2022-06-04 21:57:45 +0200

    [ItemDefinitions] Remove the inventory type property

--- item_definitions.gd
+++ item_definitions.gd
@@ -1,8 +1,6 @@
 class_name ItemDefinitions
 extends Resource
 
-const KEY_INV_TYPE: String = "inventory_type";
-const KEY_ITEM_PROTOTYPES: String = "items_prototypes";
 const KEY_ID: String = "id";
 
 export(String, MULTILINE) var json_data setget _set_json_data;
@@ -20,19 +18,10 @@ func _set_json_data(new_json_data: String) -> void:
 func parse(json: String) -> void:
     definitions.clear();
 
-    var def = parse_json(json);
-    assert(def is Dictionary, "JSON file must contain an object!");
+    var prototypes = parse_json(json);
+    assert(prototypes is Array, "JSON file must contain an array!");
 
-    assert(def.has(KEY_INV_TYPE), "Item definitions must have a '%s' property" % KEY_INV_TYPE);
-    inventory_type = def[KEY_INV_TYPE];
-    assert(!inventory_type.empty(), "Invalid inventory type (empty string)!");
-    assert(def.has(KEY_ITEM_PROTOTYPES), \
-        "Item definition must have an '%s' property!" % KEY_ITEM_PROTOTYPES);
-
-    var items = def[KEY_ITEM_PROTOTYPES];
-    assert(items is Array, "'%s' property must be an array!" % KEY_ITEM_PROTOTYPES);
-
-    for item_def in items:
+    for item_def in prototypes:
         assert(item_def is Dictionary, "Item definition must be a dictionary!");
         assert(item_def.has(KEY_ID), "Item definition must have an '%s' property!" % KEY_ID);
         assert(item_def[KEY_ID] is String, "'%s' property must be a string!" % KEY_ID);
