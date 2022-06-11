6179e61edea9c08b7a5abe7a69db1ae57e81c404
Peter Kish <pkish023@gmail.com>
2022-06-11 23:48:51 +0200

    [InventoryItem] Removed some unused methods

--- inventory_item.gd
+++ inventory_item.gd
@@ -10,18 +10,6 @@ func get_inventory() -> Node:
     return get_parent();
 
 
-func get_prototype() -> Dictionary:
-    assert(protoset, "Item protoset must be set in order to get its prototype!");
-    assert(!prototype_id.empty(), "Item prototype_id must be set in order to get its prototype!");
-    return protoset.get(prototype_id);
-
-
-func get_prototype_property(property_name: String, default_value = null):
-    if get_prototype().has(property_name):
-        return get_prototype()[property_name];
-    return default_value;
-
-
 func get_property(property_name: String, default_value = null):
     if properties.has(property_name):
         return properties[property_name];
