48df6a4dfd0fa0d919efbec038f4f33117d50cbb
Peter Kish <pkish023@gmail.com>
2022-07-01 23:03:38 +0200

    [Examples] Added CtrlItemSlot to inventory_stacked_transfer

--- examples/inventory_transfer.gd
+++ examples/inventory_transfer.gd
@@ -5,18 +5,18 @@ onready var inventory_left: Inventory = $Inventory
 onready var inventory_right: Inventory = $Inventory2
 onready var btn_left_to_right: Button = $VBoxContainer/HBoxContainer2/BtnLToR
 onready var btn_right_to_left: Button = $VBoxContainer/HBoxContainer2/BtnRToL
-onready var btn_equip_left: Button = $VBoxContainer/HBoxContainer3/HBoxContainer/BtnEquipL
-onready var btn_unequip_left: Button = $VBoxContainer/HBoxContainer3/HBoxContainer/BtnUnequipL
+onready var btn_equip: Button = $VBoxContainer/HBoxContainer3/HBoxContainer/BtnEquipL
+onready var btn_unequip: Button = $VBoxContainer/HBoxContainer3/HBoxContainer/BtnUnequipL
 onready var ctrl_inventory_left: CtrlInventory = $VBoxContainer/HBoxContainer/CtrlInventory
 onready var ctrl_inventory_right: CtrlInventory = $VBoxContainer/HBoxContainer/CtrlInventory2
-onready var slot_left: ItemSlot = $ItemSlot
+onready var slot: ItemSlot = $ItemSlot
 
 
 func _ready() -> void:
     btn_left_to_right.connect("pressed", self, "_on_ltor_pressed")
     btn_right_to_left.connect("pressed", self, "_on_rtol_pressed")
-    btn_equip_left.connect("pressed", self, "_on_equip_pressed")
-    btn_unequip_left.connect("pressed", self, "_on_unequip_pressed")
+    btn_equip.connect("pressed", self, "_on_equip_pressed")
+    btn_unequip.connect("pressed", self, "_on_unequip_pressed")
 
 
 func _on_ltor_pressed() -> void:
@@ -42,8 +42,8 @@ func _on_equip_pressed() -> void:
     if items.empty():
         return
 
-    slot_left.item = items[0]
+    slot.item = items[0]
 
 
 func _on_unequip_pressed() -> void:
-    slot_left.item = null
+    slot.item = null
