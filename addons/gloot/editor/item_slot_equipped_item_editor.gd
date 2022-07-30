92a4ced7092575d6e4bf02e99f50a436045911bb
Peter Kish <pkish023@gmail.com>
2022-07-30 22:58:24 +0200

    [Inspector Plugin] ItemSlot property editor bugfixes

--- addons/gloot/editor/item_slot_equipped_item_editor.gd
+++ addons/gloot/editor/item_slot_equipped_item_editor.gd
@@ -1,13 +1,10 @@
 extends EditorProperty
 
-var current_value: Dictionary
 var updating: bool = false
 var _option_button: OptionButton
 
 
 func _init() -> void:
-    rect_min_size.y = 200
-
     _option_button = OptionButton.new()
     add_child(_option_button)
     add_focusable(_option_button)
@@ -32,6 +29,7 @@ func _refresh_option_button() -> void:
 func _clear_option_button() -> void:
     _option_button.clear()
     _option_button.add_item("NONE")
+    _option_button.set_item_metadata(0, -1)
     _option_button.select(0)
 
 
@@ -44,10 +42,11 @@ func _populate_option_button() -> void:
         return
 
     var selected_item_index = 0
-    for item in item_slot.inventory.get_items():
+    for inventory_item_index in range(item_slot.inventory.get_items().size()):
+        var item = item_slot.inventory.get_items()[inventory_item_index]
         _option_button.add_item(item.prototype_id)
         var current_item_index = _option_button.get_item_count() - 1
-        _option_button.set_item_metadata(current_item_index, item)
+        _option_button.set_item_metadata(current_item_index, inventory_item_index)
         if item == item_slot.item:
             selected_item_index = current_item_index
 
@@ -58,7 +57,10 @@ func _on_item_selected(item_index: int) -> void:
     if !get_edited_object():
         return
 
-    var item_slot: ItemSlot = get_edited_object()
-    item_slot.item = _option_button.get_item_metadata(item_index)
+    if updating:
+        return
 
-    print("_on_item_selected: %d" % item_index)
+    updating = true
+    var item_slot: ItemSlot = get_edited_object()
+    item_slot.equipped_item = _option_button.get_item_metadata(item_index)
+    updating = false
