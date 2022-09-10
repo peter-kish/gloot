41b412b472bcaf13d1367cc9344a515c3b539c6c
Peter Kish <pkish023@gmail.com>
2022-09-10 22:39:41 +0200

    [Inspector Plugin] Refactoring the value editor

--- addons/gloot/editor/value_editor.gd
+++ addons/gloot/editor/value_editor.gd
@@ -2,7 +2,7 @@ extends MarginContainer
 
 signal value_changed
 
-const MultifloadEditor = preload("res://addons/gloot/editor/multifloat_editor.gd")
+const MultifloatEditor = preload("res://addons/gloot/editor/multifloat_editor.gd")
 
 var value setget _set_value
 
@@ -28,20 +28,23 @@ func _clear() -> void:
 
 func _add_control() -> void:
     var type = typeof(value)
+    var control: Control = null
 
     match type:
         TYPE_COLOR:
-            _create_color_picker()
+            control = _create_color_picker()
         TYPE_BOOL:
-            _create_checkbox()
+            control = _create_checkbox()
         TYPE_VECTOR2:
-            _create_v2_editor()
+            control = _create_v2_editor()
         TYPE_VECTOR3:
-            _create_v3_editor()
+            control = _create_v3_editor()
         TYPE_RECT2:
-            _create_r2_editor()
+            control = _create_r2_editor()
         _:
-            _create_line_edit()
+            control = _create_line_edit()
+
+    add_child(control)
 
 
 func _create_line_edit() -> LineEdit:
@@ -49,7 +52,6 @@ func _create_line_edit() -> LineEdit:
     line_edit.text = var2str(value)
     _expand_control(line_edit)
     line_edit.connect("text_entered", self, "_on_line_edit_value_entered", [line_edit])
-    add_child(line_edit)
     return line_edit
 
 
@@ -67,7 +69,6 @@ func _create_color_picker() -> ColorPickerButton:
     picker.color = value
     _expand_control(picker)
     picker.connect("popup_closed", self, "_on_color_picked", [picker])
-    add_child(picker)
     return picker
 
 
@@ -81,7 +82,6 @@ func _create_checkbox() -> CheckButton:
     checkbox.pressed = value
     _expand_control(checkbox)
     checkbox.connect("pressed", self, "_on_checkbox", [checkbox])
-    add_child(checkbox)
     return checkbox
 
 
@@ -91,13 +91,9 @@ func _on_checkbox(checkbox: CheckButton) -> void:
 
 
 func _create_v2_editor() -> Control:
-    var v2_editor = MultifloadEditor.new()
-    v2_editor.columns = 2
-    v2_editor.values = [value.x, value.y]
-    v2_editor.titles = ["X", "Y"]
-    _expand_control(v2_editor)
-    v2_editor.connect("value_changed", self, "_on_v2_value_changed", [v2_editor])
-    add_child(v2_editor)
+    var values = [value.x, value.y]
+    var titles = ["X", "Y"]
+    var v2_editor = _create_multifloat_editor(2, values, titles, "_on_v2_value_changed")
     return v2_editor
 
 
@@ -108,13 +104,9 @@ func _on_v2_value_changed(_idx: int, v2_editor: Control) -> void:
 
 
 func _create_v3_editor() -> Control:
-    var v3_editor = MultifloadEditor.new()
-    v3_editor.columns = 3
-    v3_editor.values = [value.x, value.y, value.z]
-    v3_editor.titles = ["X", "Y", "Z"]
-    _expand_control(v3_editor)
-    v3_editor.connect("value_changed", self, "_on_v3_value_changed", [v3_editor])
-    add_child(v3_editor)
+    var values = [value.x, value.y, value.z]
+    var titles = ["X", "Y", "Z"]
+    var v3_editor = _create_multifloat_editor(3, values, titles, "_on_v3_value_changed")
     return v3_editor
 
 
@@ -126,22 +118,32 @@ func _on_v3_value_changed(_idx: int, v3_editor: Control) -> void:
 
 
 func _create_r2_editor() -> Control:
-    var r2_editor = MultifloadEditor.new()
-    r2_editor.columns = 2
-    r2_editor.values = [value.position.x, value.position.y, value.size.x, value.size.y]
-    r2_editor.titles = ["Position X", "Position Y", "Size X", "Size Y"]
-    _expand_control(r2_editor)
-    r2_editor.connect("value_changed", self, "_on_r2_value_changed", [r2_editor])
-    add_child(r2_editor)
+    var values = [value.position.x, value.position.y, value.size.x, value.size.y]
+    var titles = ["Position X", "Position Y", "Size X", "Size Y"]
+    var r2_editor = _create_multifloat_editor(2, values, titles, "_on_r2_value_changed")
     return r2_editor
 
 
 func _on_r2_value_changed(_idx: int, r2_editor: Control) -> void:
     value.position.x = r2_editor.values[0]
-    value.position.y = r2_editor.values[0]
-    value.size.x = r2_editor.values[0]
-    value.size.y = r2_editor.values[0]
+    value.position.y = r2_editor.values[1]
+    value.size.x = r2_editor.values[2]
+    value.size.y = r2_editor.values[3]
     emit_signal("value_changed")
+
+
+func _create_multifloat_editor(
+        columns: int,
+        values: Array,
+        titles: Array,
+        value_changed_handler: String) -> Control:
+    var multifloat_editor = MultifloatEditor.new()
+    multifloat_editor.columns = columns
+    multifloat_editor.values = values
+    multifloat_editor.titles = titles
+    _expand_control(multifloat_editor)
+    multifloat_editor.connect("value_changed", self, value_changed_handler, [multifloat_editor])
+    return multifloat_editor
 
 
 func _expand_control(c: Control) -> void:
