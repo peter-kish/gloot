745107171afa2a4c8366653b945d2e2167cd51c5
Peter Kish <pkish023@gmail.com>
2022-09-16 22:33:58 +0200

    [Inspector Plugin] Added an option to expand the inventory editor

--- addons/gloot/editor/inventory_inspector.gd
+++ addons/gloot/editor/inventory_inspector.gd
@@ -1,7 +1,11 @@
 extends Control
 tool
 
+const InventoryEditor = preload("res://addons/gloot/editor/inventory_editor.tscn")
 const EditorIcons = preload("res://addons/gloot/editor/editor_icons.gd")
+const POPUP_SIZE = Vector2(800, 600)
+const POPUP_MIN_SIZE = Vector2(400, 300)
+const POPUP_MARGIN = 10
 
 onready var inventory_editor: Control = $HBoxContainer/InventoryEditor
 onready var btn_expand: Button = $HBoxContainer/BtnExpand
@@ -9,6 +13,8 @@ onready var btn_expand: Button = $HBoxContainer/BtnExpand
 var inventory: Inventory setget _set_inventory
 var editor_interface: EditorInterface setget _set_editor_interface
 var gloot_undo_redo setget _set_undo_redo
+var _window_dialog: WindowDialog
+var _popup_inventory_editor: Control
 
 
 func _set_inventory(new_inventory: Inventory) -> void:
@@ -29,6 +35,29 @@ func _set_undo_redo(new_gloot_undo_redo) -> void:
         inventory_editor.gloot_undo_redo = gloot_undo_redo
 
 
+func _init() -> void:
+    _window_dialog = WindowDialog.new()
+    _window_dialog.window_title = "Edit Inventory"
+    _window_dialog.resizable = true
+    _window_dialog.rect_size = POPUP_SIZE
+    _window_dialog.rect_min_size = POPUP_MIN_SIZE
+    add_child(_window_dialog)
+
+    _popup_inventory_editor = InventoryEditor.instance()
+
+    var _margin_container = MarginContainer.new()
+    _margin_container.margin_bottom = -POPUP_MARGIN
+    _margin_container.margin_left = POPUP_MARGIN
+    _margin_container.margin_right = -POPUP_MARGIN
+    _margin_container.margin_top = POPUP_MARGIN
+    _margin_container.size_flags_horizontal = SIZE_EXPAND_FILL
+    _margin_container.size_flags_vertical = SIZE_EXPAND_FILL
+    _margin_container.anchor_bottom = 1.0
+    _margin_container.anchor_right = 1.0
+    _margin_container.add_child(_popup_inventory_editor)
+    _window_dialog.add_child(_margin_container)
+
+
 func _ready() -> void:
     if inventory_editor:
         inventory_editor.inventory = inventory
@@ -36,6 +65,14 @@ func _ready() -> void:
         inventory_editor.gloot_undo_redo = gloot_undo_redo
     _apply_editor_settings()
     btn_expand.icon = EditorIcons.get_icon(editor_interface, "DistractionFree")
+    btn_expand.connect("pressed", self, "on_btn_expand")
+
+
+func on_btn_expand() -> void:
+    _popup_inventory_editor.inventory = inventory
+    _popup_inventory_editor.gloot_undo_redo = gloot_undo_redo
+    _popup_inventory_editor.editor_interface = editor_interface
+    _window_dialog.popup_centered(POPUP_SIZE)
 
 
 func _apply_editor_settings() -> void:
