09b7331200741e5dbea315c5731e964494ef94c3
Peter Kish <pkish023@gmail.com>
2023-10-08 12:00:13 +0200

    [GlootInventoryGrid] WIP

---
+++ addons/gloot/ui/gloot_inventory_field.gd
@@ -0,0 +1,55 @@
+@tool
+extends Panel
+
+@export var style: StyleBox :
+    get:
+        return style
+    set(new_style):
+        style = new_style
+        if !selected:
+            _set_active_style(style)
+
+@export var hover_style: StyleBox :
+    get:
+        return hover_style
+    set(new_hover_style):
+        hover_style = new_hover_style
+
+@export var selected_style: StyleBox :
+    get:
+        return selected_style
+    set(new_selected_style):
+        selected_style = new_selected_style
+        if selected:
+            _set_active_style(selected_style)
+
+@export var selected: bool :
+    get:
+        return selected
+    set(new_selected):
+        selected = new_selected
+        if selected:
+            _set_active_style(selected_style)
+        else:
+            _set_active_style(style)
+
+
+func _set_active_style(style: StyleBox) -> void:
+    remove_theme_stylebox_override("panel")
+    if style != null:
+        add_theme_stylebox_override("panel", style)
+
+
+func _ready() -> void:
+    mouse_entered.connect(_on_mouse_entered)
+    mouse_exited.connect(_on_mouse_exited)
+
+
+func _on_mouse_entered() -> void:
+    if !selected:
+        _set_active_style(hover_style)
+
+
+func _on_mouse_exited() -> void:
+    if !selected:
+        _set_active_style(style)
