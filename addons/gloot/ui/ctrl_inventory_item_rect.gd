a2af1c88e30af13fb0a71bc4f4688427bda40959
Peter Kis <pkish023@gmail.com>
2024-02-02 22:45:01 +0100

    [CtrlInventoryItemRect] Refactoring

--- addons/gloot/ui/ctrl_inventory_item_rect.gd
+++ addons/gloot/ui/ctrl_inventory_item_rect.gd
@@ -23,22 +23,23 @@ var texture: Texture2D :
         if new_texture == texture:
             return
         texture = new_texture
-        if texture != null:
-            size = texture.get_size()
-        queue_redraw()
+        _update_texture()
 var selected: bool = false :
     set(new_selected):
         if new_selected == selected:
             return
         selected = new_selected
-        queue_redraw()
+        _update_selection()
 var selection_bg_color: Color = Color.GRAY :
     set(new_selection_bg_color):
         if new_selection_bg_color == selection_bg_color:
             return
         selection_bg_color = new_selection_bg_color
-        queue_redraw()
+        _update_selection()
 var item_slot: ItemSlot
+var _selection_rect: ColorRect
+var _texture_rect: TextureRect
+var _stack_size_label: Label
 static var _stored_preview_size: Vector2
 static var _stored_preview_offset: Vector2
 
@@ -47,24 +48,24 @@ func _connect_item_signals(new_item: InventoryItem) -> void:
     if new_item == null:
         return
 
-    if !new_item.protoset_changed.is_connected(queue_redraw):
-        new_item.protoset_changed.connect(queue_redraw)
-    if !new_item.prototype_id_changed.is_connected(queue_redraw):
-        new_item.prototype_id_changed.connect(queue_redraw)
-    if !new_item.properties_changed.is_connected(queue_redraw):
-        new_item.properties_changed.connect(queue_redraw)
+    if !new_item.protoset_changed.is_connected(_refresh):
+        new_item.protoset_changed.connect(_refresh)
+    if !new_item.prototype_id_changed.is_connected(_refresh):
+        new_item.prototype_id_changed.connect(_refresh)
+    if !new_item.properties_changed.is_connected(_refresh):
+        new_item.properties_changed.connect(_refresh)
 
 
 func _disconnect_item_signals() -> void:
     if item == null:
         return
 
-    if item.protoset_changed.is_connected(queue_redraw):
-        item.protoset_changed.disconnect(queue_redraw)
-    if item.prototype_id_changed.is_connected(queue_redraw):
-        item.prototype_id_changed.disconnect(queue_redraw)
-    if item.properties_changed.is_connected(queue_redraw):
-        item.properties_changed.disconnect(queue_redraw)
+    if item.protoset_changed.is_connected(_refresh):
+        item.protoset_changed.disconnect(_refresh)
+    if item.prototype_id_changed.is_connected(_refresh):
+        item.prototype_id_changed.disconnect(_refresh)
+    if item.properties_changed.is_connected(_refresh):
+        item.properties_changed.disconnect(_refresh)
 
 
 func _get_item_size() -> Vector2:
@@ -81,6 +82,52 @@ func _get_item_position() -> Vector2:
 
 func _ready() -> void:
     drag_preview = CtrlInventoryItemRect.new()
+
+    _selection_rect = ColorRect.new()
+    _texture_rect = TextureRect.new()
+    _stack_size_label = Label.new()
+    add_child(_selection_rect)
+    add_child(_texture_rect)
+    add_child(_stack_size_label)
+
+    resized.connect(func():
+        _selection_rect.size = size
+        _texture_rect.size = size
+        _stack_size_label.size = size
+    )
+
+    _refresh()
+
+func _update_selection() -> void:
+    if _selection_rect == null:
+        return
+    _selection_rect.visible = selected
+    _selection_rect.color = selection_bg_color
+    _selection_rect.size = size
+
+
+func _update_texture() -> void:
+    if _texture_rect == null:
+        return
+    _texture_rect.texture = texture
+    _texture_rect.size = size
+
+
+func _update_stack_size() -> void:
+    if _stack_size_label == null:
+        return
+    var stack_size: int = StacksConstraint.get_item_stack_size(item)
+    if stack_size <= 1:
+        _stack_size_label.text = ""
+    else:
+        _stack_size_label.text = "%d" % stack_size
+    _stack_size_label.size = size
+
+
+func _refresh() -> void:
+    _update_selection()
+    _update_texture()
+    _update_stack_size()
 
 
 func drag_start() -> void:
@@ -111,47 +158,6 @@ static func restore_preview_size() -> void:
         return
     _grabbed_ctrl.drag_preview.size = _stored_preview_size
     CtrlDragable._grab_offset = _stored_preview_offset
-
-
-func _draw() -> void:
-    if is_dragged():
-        return
-    var rect = Rect2(Vector2.ZERO, size)
-    _draw_selection(rect)
-    _draw_texture(rect)
-    _draw_stack_size(rect)
-
-
-func _draw_selection(rect: Rect2):
-    if selected:
-        draw_rect(rect, selection_bg_color, true)
-
-
-func _draw_texture(rect: Rect2):
-    if texture:
-        var src_rect: Rect2 = Rect2(0, 0, texture.get_width(), texture.get_height())
-        draw_texture_rect_region(texture, rect, src_rect)
-
-
-func _draw_stack_size(rect: Rect2):
-    if item == null:
-        return
-
-    var stack_size: int = StacksConstraint.get_item_stack_size(item)
-    if stack_size <= 1:
-        return
-
-    var default_font := ThemeDB.fallback_font
-    var default_font_size := ThemeDB.fallback_font_size
-    var text = str(stack_size)
-    draw_string(
-        default_font,
-        rect.position + Vector2(0, rect.size.y),
-        text,
-        HORIZONTAL_ALIGNMENT_RIGHT,
-        rect.size.x, 
-        default_font_size
-    )
 
 
 func _gui_input(event: InputEvent) -> void:
