aee19c377b361c6d246332cc082f9600b260687d
Peter Kis <pkish023@gmail.com>
2024-06-06 21:45:01 +0200

    Typo

--- addons/gloot/core/utils.gd
+++ addons/gloot/core/utils.gd
@@ -7,9 +7,10 @@ static func safe_disconnect(s: Signal, c: Callable) -> void:
     if s.is_connected(c):
         s.disconnect(c)
 
+
 static func str_to_var(s: String) -> Variant:
     var variant = str_to_var(s)
-    # str_to_var considers all strings that start with a digit convertable to
+    # str_to_var considers all strings that start with a digit convertible to
     # int/float (which is not consistent with String.is_valid_int and
     # String.is_valid_float).
     if typeof(variant) == TYPE_INT && !s.is_valid_int():
