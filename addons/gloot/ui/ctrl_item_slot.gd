82320d2049cae1ab2e8fe5d2f9af7df44b8e8516
Peter Kis <pkish023@gmail.com>
2024-06-09 11:57:54 +0200

    [CtrlItemSlot] Removed item label

--- addons/gloot/ui/ctrl_item_slot.gd
+++ addons/gloot/ui/ctrl_item_slot.gd
@@ -24,23 +24,6 @@ const Utils = preload("res://addons/gloot/core/utils.gd")
         
         _refresh()
 
-## If set to `true`, the item icon is displayed.
-@export var item_texture_visible: bool = true :
-    set(new_item_texture_visible):
-        if item_texture_visible == new_item_texture_visible:
-            return
-        item_texture_visible = new_item_texture_visible
-        if is_instance_valid(_ctrl_draggable_inventory_item):
-            _ctrl_draggable_inventory_item.visible = item_texture_visible
-## If set to `true`, the item name label is displayed.
-@export var label_visible: bool = true :
-    set(new_label_visible):
-        if label_visible == new_label_visible:
-            return
-        label_visible = new_label_visible
-        if is_instance_valid(_label):
-            _label.visible = label_visible
-
 @export_group("Icon Behavior", "icon_")
 ## Controls the item icon behavior when resizing the node's bounding rectangle. See the `TextureRect.StretchMode`
 ## constants for details.
@@ -52,56 +35,24 @@ const Utils = preload("res://addons/gloot/core/utils.gd")
         if is_instance_valid(_ctrl_draggable_inventory_item):
             _ctrl_draggable_inventory_item.stretch_mode = icon_stretch_mode
 
-@export_group("Text Behavior", "label_")
-## Controls the label's horizontal alignment.
-@export var label_horizontal_alignment: HorizontalAlignment = HORIZONTAL_ALIGNMENT_CENTER :
-    set(new_label_horizontal_alignment):
-        if label_horizontal_alignment == new_label_horizontal_alignment:
-            return
-        label_horizontal_alignment = new_label_horizontal_alignment
-        if is_instance_valid(_label):
-            _label.horizontal_alignment = label_horizontal_alignment
-## Controls the label's vertical alignment.
-@export var label_vertical_alignment: VerticalAlignment = VERTICAL_ALIGNMENT_CENTER :
-    set(new_label_vertical_alignment):
-        if label_vertical_alignment == new_label_vertical_alignment:
-            return
-        label_vertical_alignment = new_label_vertical_alignment
-        if is_instance_valid(_label):
-            _label.vertical_alignment = label_vertical_alignment
-## Sets the clipping behavior when the text exceeds the node's bounding rectangle.
-@export var label_text_overrun_behavior: TextServer.OverrunBehavior :
-    set(new_label_text_overrun_behavior):
-        if label_text_overrun_behavior == new_label_text_overrun_behavior:
-            return
-        label_text_overrun_behavior = new_label_text_overrun_behavior
-        if is_instance_valid(_label):
-            _label.text_overrun_behavior = label_text_overrun_behavior
-## If `true`, the label only shows the text that fits inside its bounding rectangle and will clip text horizontally.
-@export var label_clip_text: bool :
-    set(new_label_clip_text):
-        if label_clip_text == new_label_clip_text:
-            return
-        label_clip_text = new_label_clip_text
-        if is_instance_valid(_label):
-            _label.clip_text = label_clip_text
-
 @export_group("Custom Styles")
 ## The slot background style.
 @export var slot_style: StyleBox :
     set(new_slot_style):
+        if slot_style == new_slot_style:
+            return
         slot_style = new_slot_style
         _refresh()
 ## The slot background style when the mouse cursor hovers over the slot.
 @export var slot_highlighted_style: StyleBox :
     set(new_slot_highlighted_style):
+        if slot_highlighted_style == new_slot_highlighted_style:
+            return
         slot_highlighted_style = new_slot_highlighted_style
         _refresh()
 
 var _background_panel: Panel
-var _hbox_container: HBoxContainer
 var _ctrl_draggable_inventory_item: CtrlDraggableInventoryItem
-var _label: Label
 var _ctrl_drop_zone: CtrlDropZone
 
 
@@ -126,43 +77,31 @@ func _ready():
             remove_child(c)
             c.queue_free()
 
-    _hbox_container = HBoxContainer.new()
-    _hbox_container.size_flags_horizontal = SIZE_EXPAND_FILL
-    _hbox_container.size_flags_vertical = SIZE_EXPAND_FILL
-    add_child(_hbox_container)
-    _hbox_container.resized.connect(func(): size = _hbox_container.size)
+    _background_panel = Panel.new()
+    _background_panel.size = size
+    _set_panel_style(_background_panel, slot_style)
+    add_child(_background_panel)
 
     _ctrl_draggable_inventory_item = CtrlDraggableInventoryItem.new()
-    _ctrl_draggable_inventory_item.visible = item_texture_visible
-    _ctrl_draggable_inventory_item.size_flags_horizontal = SIZE_EXPAND_FILL
-    _ctrl_draggable_inventory_item.size_flags_vertical = SIZE_EXPAND_FILL
     _ctrl_draggable_inventory_item.icon_stretch_mode = icon_stretch_mode
-    _hbox_container.add_child(_ctrl_draggable_inventory_item)
+    _ctrl_draggable_inventory_item.size = size
+    add_child(_ctrl_draggable_inventory_item)
 
     _ctrl_drop_zone = CtrlDropZone.new()
     _ctrl_drop_zone.draggable_dropped.connect(_on_draggable_dropped)
     _ctrl_drop_zone.size = size
-    resized.connect(func(): _ctrl_drop_zone.size = size)
     CtrlDraggable.draggable_grabbed.connect(_on_any_draggable_grabbed)
     CtrlDraggable.draggable_dropped.connect(_on_any_draggable_dropped)
     add_child(_ctrl_drop_zone)
     _ctrl_drop_zone.deactivate()
 
-    _label = Label.new()
-    _label.visible = label_visible
-    _label.size_flags_horizontal = SIZE_EXPAND_FILL
-    _label.size_flags_vertical = SIZE_EXPAND_FILL
-    _label.horizontal_alignment = label_horizontal_alignment
-    _label.vertical_alignment = label_vertical_alignment
-    _label.text_overrun_behavior = label_text_overrun_behavior
-    _label.clip_text = label_clip_text
-    _hbox_container.add_child(_label)
-
-    _hbox_container.size = size
     resized.connect(func():
-        _hbox_container.size = size
         if is_instance_valid(_background_panel):
             _background_panel.size = size
+        if is_instance_valid(_ctrl_drop_zone):
+            _ctrl_drop_zone.size = size
+        if is_instance_valid(_ctrl_draggable_inventory_item):
+            _ctrl_draggable_inventory_item.size = size
     )
 
     _refresh()
@@ -223,8 +162,6 @@ func _notification(what: int) -> void:
 func _refresh() -> void:
     _clear()
 
-    _update_background()
-
     if !is_instance_valid(item_slot):
         return
 
@@ -232,31 +169,13 @@ func _refresh() -> void:
     if !is_instance_valid(item):
         return
         
-    if is_instance_valid(_label):
-        _label.text = item.get_property(InventoryItem.KEY_NAME, item.get_title())
     if is_instance_valid(_ctrl_draggable_inventory_item):
         _ctrl_draggable_inventory_item.item = item
 
 
 func _clear() -> void:
-    if is_instance_valid(_label):
-        _label.text = ""
     if is_instance_valid(_ctrl_draggable_inventory_item):
         _ctrl_draggable_inventory_item.item = null
-
-
-func _update_background() -> void:
-    if !is_instance_valid(_background_panel):
-        _background_panel = Panel.new()
-        add_child(_background_panel)
-        move_child(_background_panel, 0)
-        
-    _background_panel.size = size
-    _background_panel.show()
-    if slot_style:
-        _set_panel_style(_background_panel, slot_style)
-    else:
-        _background_panel.hide()
 
 
 func _set_panel_style(panel: Panel, style: StyleBox) -> void:
